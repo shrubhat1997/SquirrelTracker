30ed598418d174b26cbde51442b0022c743c33f6
shrubhat1997 <ssb2203@columbia.edu>
2021-04-12 05:00:56 +0000

    updating views.py

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Squirrel
+
+def index(request) :
+    squirrels = Squirrel.objects.all()
+    context = {
+        'squirrels' : squirrels,
+    }
+
+    return render(request,'sightings/index.html',context)
