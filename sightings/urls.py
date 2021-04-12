3a44a0334ba85ec8689a1ce882840bb2b11e7da9
Chloschmo <cw2684@columbia.edu>
2021-04-12 20:57:23 +0000

    Add add and update features to views and linked changes

--- sightings/urls.py
+++ sightings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
+
 urlpatterns = [
         path('sightings/', views.index),
         path('maps/',views.view_map, name='view_map'),
+        path('sightings/<Unique_Squirrel_ID>/', views.update, name='update'),
+        path('sightings/add/', views.add, name='add'),
     ]
--- sightings/views.py
+++ sightings/views.py
@@ -2,15 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Squirrels
 from django.shortcuts import redirect, get_object_or_404
+
+from .models import Squirrel
+from .forms import SquirrelSightingForm
+
 def index(request) :
     squirrels = Squirrels.objects.all()
     context = {
         'squirrels' : squirrels,
     }
     return render(request,'sightings/index.html',context)
+
 def view_map(request) :
     spot = Squirrels.objects.all()
     context = {
         'spot' : spot,
     }
     return render(request,'sightings/maps.html',context)
+
+def update(request, Unique_Squirrel_ID):
+    object = get_object_or_404(Squirrel, Unique_squirrel_id=Unique_Squirrel_ID)
+    if request.method=='POST':
+        form = SquirrelSightingForm(request.POST, instance=object)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/{Unique_Squirrel_ID}')
+        else:
+            form = SquirrelSightingForm(instance=object)
+            context = {
+                    'form': form;
+                    }
+    return render(request, 'sightings/squirrel_update.html', context)
+
+def add(request):
+        if request.method=='POST':
+            form = SquirrelSightingForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect(f'/sightings/')
+        else:
+            form = SquirrelSightingForm()
+        context = {
+                    'form': form;
+                    }
+        return render(request, 'sightings/squirrel_add.html', context)
+
