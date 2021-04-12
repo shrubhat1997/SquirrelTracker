f7e21aa6c2539fbedbe5902628ec5e10629a4942
shrubhat1997 <ssb2203@columbia.edu>
2021-04-12 19:19:30 +0000

    adding index and map views

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Squirrels
+from django.shortcuts import redirect, get_object_or_404
+def index(request) :
+    squirrels = Squirrels.objects.all()
+    context = {
+        'squirrels' : squirrels,
+    }
+    return render(request,'sightings/index.html',context)
+def view_map(request) :
+    spot = Squirrels.objects.all()
+    context = {
+        'spot' : spot,
+    }
+    return render(request,'sightings/maps.html',context)
